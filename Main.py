4c6157c4cd321dd8978bd00c3f8a02408a49faaf
KyleCotton <kylecottonkc@gmail.com>
2017-08-07 22:10:26 +0100

    Created SystemsTestNew
    System test new is the new version of the code, runs with better efficency. The arguments you pass also simplified.

--- Main.py
+++ Main.py
@@ -4,13 +4,8 @@
 # Version:     1.0
 #-------------------------------------------------------------------------------
 
-#Pulling in systems test File
-from SystemsTest import *
+from SystemsTestNew import *
 
-"""
-If the systems function is passed "all" all systes will be checked without
-further user input
-"""
 #Test all systems
-SystemsTest("ALL")
-
+SystemsTest("help")
+SystemsTest()
